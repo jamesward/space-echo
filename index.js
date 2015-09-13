d3cb1593e77563f222f35713cac01bb163000eaf
James Ward <james@jamesward.org>
2015-09-12 21:26:46 -0600

    stuff

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var os = require('os');
 var dns = require('dns');
+var url = require('url');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,13 +13,15 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
+  var databaseHost = url.parse(process.env.DATABASE_URL).host;
   dns.lookup(request.headers.host, function onLookup(err, addresses, family) {
     response.render('pages/index', {
+      "host": request.headers.host,
       "router": addresses,
-      "internal": os.networkInterfaces()['eth1'][0]['address']
+      "internal": os.networkInterfaces()['eth1'][0]['address'],
+      "database": databaseHost
     });
   });
-
 });
 
 app.listen(app.get('port'), function() {
